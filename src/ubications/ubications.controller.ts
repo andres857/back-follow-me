834821b2fae3fb19788d85b72ba66a519687c263
Andres Guerrero <andres-guerrero@Andress-MacBook-Air.local>
2024-01-11 06:59:27 -0500

    changes to office

--- src/ubications/ubications.controller.ts
+++ src/ubications/ubications.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { UbicationsService } from './ubications.service';
+import { CreateUbicationDto } from './dto/ubication.dto';
 
 @Controller('ubications')
-export class UbicationsController {}
+export class UbicationsController {
+  constructor(private ubicationService: UbicationsService) {}
+
+  @Get()
+  async getAll() {
+    const ubications = await this.ubicationService.findAll();
+    console.log(ubications);
+
+    return ubications;
+  }
+
+  @Post()
+  async create(@Body() createLocationDto: CreateUbicationDto) {
+    return this.ubicationService.create(createLocationDto);
+  }
+}
--- src/ubications/ubications.module.ts
+++ src/ubications/ubications.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Ubication } from './entity/ubication.entity';
 import { UbicationsController } from './ubications.controller';
 import { UbicationsService } from './ubications.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Ubication])],
   controllers: [UbicationsController],
-  providers: [UbicationsService]
+  providers: [UbicationsService],
 })
 export class UbicationsModule {}
