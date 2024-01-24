dfd87731ecd872d3fa17df50fc647e5177faa9bb
Andres Guerrero <andres-guerrero@Andress-MacBook-Air.local>
2024-01-23 19:20:25 -0500

    se agrega el manejo a las solicitudes http en ubicaciones

--- src/ubications/ubications.controller.ts
+++ src/ubications/ubications.controller.ts
@@ -7,9 +7,12 @@ import {
   UploadedFile,
   UseInterceptors,
   Req,
+  HttpStatus,
+  Res,
 } from '@nestjs/common';
 import { UbicationsService } from './ubications.service';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { Response } from 'express';
 
 @Controller('ubications')
 export class UbicationsController {
@@ -21,23 +24,38 @@ export class UbicationsController {
     return ubication;
   }
 
-  @Post()
-  async create(@Body() createLocationDto: any) {
-    return this.ubicationService.create(createLocationDto);
-  }
+  // @Post()
+  // async create(@Body() createLocationDto: any) {
+  //   return this.ubicationService.create(createLocationDto);
+  // }
 
   @Post('/new/upload')
   @UseInterceptors(FileInterceptor('file'))
-  async upload(@UploadedFile() file: any, @Req() req) {
+  async create(
+    @UploadedFile() file: any,
+    @Req() req,
+    @Res() response: Response,
+  ) {
     const payload = {
       ...req.body,
     };
-
-    const newUbication = await this.ubicationService.createUbication(payload, file);
-    console.log('---------------');
-    console.log(newUbication);
-    console.log('---------------');
-
-    return newUbication;
+    try {
+      const newUbication = await this.ubicationService.createUbication(
+        payload,
+        file,
+      );
+      response.status(HttpStatus.CREATED).json({
+        statusCode: 200,
+        message: 'success',
+        data: payload,
+        error: '',
+      });
+    } catch (error) {
+      response.status(HttpStatus.UNPROCESSABLE_ENTITY).json({
+        message: error.response.message,
+        error: error.response.error,
+        statusCode: error.response.statusCode,
+      });
+    }
   }
 }
