2fa52458d3cafdeefa444cb346d919bb3a794656
Andreslondono <andrescamilolondonomora@gmail.com>
2024-01-29 15:27:35 -0500

    funcionalidad, de logeo y optencion del token

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,27 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserModule } from 'src/users/user.module';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './auth.guard';
 
 @Module({
-  imports:[UserModule],
+  imports:[
+    UserModule,
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '3600s' },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [
+    AuthService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard
+    }
+  ]
 })
 export class AuthModule {}
