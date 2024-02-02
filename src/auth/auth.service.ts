69063d10c1b27c533e3cf63ae3333985dfe9624e
Andres Guerrero <andres-guerrero@Andress-MacBook-Air.local>
2024-02-02 10:20:27 -0500

    minnor fix

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,35 +5,39 @@ import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-    constructor(
-        private usersService: UsersService,
-        private jwtService: JwtService,
-    ) {}
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
-    async signIn(email: string, pass: string): Promise<{access_token: string}> {
-        if (!email) {
-            throw new UnauthorizedException('Email is required');
-        }
-
-        if (!pass) {
-            throw new UnauthorizedException('Password is required');
-        }
-
-        const user = await this.usersService.findEmail(email);
-
-        if (!user) {
-            throw new UnauthorizedException('Invalid credentials');
-        }
-
-        const isMatch = await bcrypt.compare(pass, user.password);
-
-        if (!isMatch) {
-            throw new UnauthorizedException();
-        }
-        const payload = { sub: user.id, username: user.email };
-        
-        return {
-            access_token: await this.jwtService.signAsync(payload),
-        };
+  async signIn(email: string, pass: string): Promise<{ access_token: string }> {
+    if (!email) {
+      throw new UnauthorizedException('Email is required');
     }
+
+    if (!pass) {
+      throw new UnauthorizedException('Password is required');
+    }
+
+    const user = await this.usersService.findEmail(email);
+
+    if (!user) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const isMatch = await bcrypt.compare(pass, user.password); // 1 parametro pass textPlane, cryp ppassword
+
+    if (!isMatch) {
+      throw new UnauthorizedException();
+    }
+    // para generar un token
+    const payload = {
+      sub: user.id,
+      username: user.email,
+    };
+
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
+  }
 }
