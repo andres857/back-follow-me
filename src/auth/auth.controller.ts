6e5eba27e7a109cab5f4c1ae712aa2c2a438b9c9
andres857 <andres857@gmail.com>
2024-02-19 17:03:02 -0500

    minnor change

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Post, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { Public } from './constants';
 import { Response } from 'express';
+import { Public } from './constants';
 
 @Public()
 @Controller('auth')
