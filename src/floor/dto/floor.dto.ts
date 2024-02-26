6083a11329fe3adb261b649807c8a82c3f2ca7eb
andres857 <andres857@gmail.com>
2024-02-25 23:36:08 -0500

    floors - update dto

--- src/floor/dto/floor.dto.ts
+++ src/floor/dto/floor.dto.ts
@@ -2,13 +2,13 @@ import { IsString, IsNotEmpty, IsNumber } from 'class-validator';
 import { PartialType } from '@nestjs/mapped-types';
 
 export class CreateFloorDto {
-  @IsNumber()
-  @IsNotEmpty()
-  id: number;
-
   @IsString()
   @IsNotEmpty()
   name: string;
 }
 
-export class UpdateFloorDto extends PartialType(CreateFloorDto) {}
+export class UpdateFloorDto extends PartialType(CreateFloorDto) {
+  @IsNumber()
+  @IsNotEmpty()
+  id: number;
+}
